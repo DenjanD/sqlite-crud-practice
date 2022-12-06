14cb7fb44c03812ee4c2e1b0447222a1ab9bbbe4
DenjanD <alfien311@gmail.com>
2022-12-06 13:53:26 +0700

    Added Minuman CRUD

--- app/src/main/java/com/denjand/sqlitecrud/Menu.java
+++ app/src/main/java/com/denjand/sqlitecrud/Menu.java
@@ -11,12 +11,17 @@ import com.denjand.sqlitecrud.view.barang.CreateData;
 import com.denjand.sqlitecrud.view.barang.ViewData;
 import com.denjand.sqlitecrud.view.makanan.CreateDataMakanan;
 import com.denjand.sqlitecrud.view.makanan.ViewDataMakanan;
+import com.denjand.sqlitecrud.view.minuman.CreateDataMinuman;
+import com.denjand.sqlitecrud.view.minuman.ViewDataMinuman;
 
 public class Menu extends AppCompatActivity implements View.OnClickListener {
     private Button bTambah;
     private Button bLihat;
     private Button bTambahMakanan;
     private Button bLihatMakanan;
+    private Button bTambahMinuman;
+    private Button bLihatMinuman;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,12 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
         bTambahMakanan.setOnClickListener(this);
         bLihatMakanan = (Button) findViewById(R.id.button_view_makanan);
         bLihatMakanan.setOnClickListener(this);
+
+        bTambahMinuman = (Button)
+                findViewById(R.id.button_tambah_minuman);
+        bTambahMinuman.setOnClickListener(this);
+        bLihatMinuman = (Button) findViewById(R.id.button_view_minuman);
+        bLihatMinuman.setOnClickListener(this);
     }
     @Override
     public void onClick(View v){
@@ -51,6 +62,14 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
             case R.id.button_view_makanan:
                 Intent viewMakanan = new Intent(this, ViewDataMakanan.class);
                 startActivity(viewMakanan);
+                break;
+            case R.id.button_tambah_minuman:
+                Intent tambahMinuman = new Intent(this, CreateDataMinuman.class);
+                startActivity(tambahMinuman);
+                break;
+            case R.id.button_view_minuman:
+                Intent viewMinuman = new Intent(this, ViewDataMinuman.class);
+                startActivity(viewMinuman);
                 break;
         }
     }
